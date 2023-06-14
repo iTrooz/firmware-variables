76d957bcdeb31cf876004c3c510d70e4e8f94bc8
iTrooz <hey@itrooz.fr>
2023-06-14 13:29:34 +0200

    raise exception when platform is not supported

--- src/firmware_variables/platform/__init__.py
+++ src/firmware_variables/platform/__init__.py
@@ -4,3 +4,5 @@ match sys.platform:
     case "win32":
         from .win32 import get_variable, set_variable, get_all_variables_names
         from .win32_privileges import adjust_privileges
+    case _:
+        raise RuntimeError("Platform not supported: "+sys.platform)
